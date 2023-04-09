0d019db6cafc64ecc9712226e3a1204d38917c2c
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-09 11:46:22 -0400

    Add section observer to title of page

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const menu = document.getElementById('menu');
+const title = document.getElementById('name');
+const topbar = document.getElementById('topbar');
+const backgroundImage = document.querySelector('#menu-background-image');
+
+
+const backgroundOptions = {};
+
+const backgroundImageObserver =  new IntersectionObserver(function(
+    entries,
+    backgroundImageObserver
+) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            title.textContent = "< AS >"
+        } else {
+            title.textContent = "Anthony Scott"
+        }
+    })
+}, backgroundOptions)
 
 Array.from(document.getElementsByClassName("menu-item"))
     .forEach((item, index) => {
@@ -6,3 +25,10 @@ Array.from(document.getElementsByClassName("menu-item"))
             menu.style.setProperty("--active-index", index)
         }
     });
+
+
+
+
+
+
+backgroundImageObserver.observe(backgroundImage);
